3cae064e87252326a1b7971bee16e0c468f590a5
Konanki Keerthi <konankikeerthi7@gmail.com>
2025-12-24 11:48:24 +0530

    basic infrastructure development completed

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,28 @@
 const express=require('express')
+const mongoose=require("mongoose")
+require("dotenv").config()
 
 const app=express()
+
+//middleware
+app.use(express.json())
+
+const connectDB=async()=>{
+    try{
+        const conn=await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Greenzen database connected ${conn.connection.host}`)
+    }
+    catch(error){
+        console.error(`Error ${error.message}`)
+        process.exit(1)
+    }
+}
+
+connectDB()
 
 app.get('/',(req,res)=>{
     res.send("app is running")
 })
 
-const port=8080
-app.listen(port,()=>console.log(`app is running on ${port}`))
+const PORT=process.env.PORT || 8000
+app.listen(PORT,()=>console.log(`app is running on ${PORT}`))
